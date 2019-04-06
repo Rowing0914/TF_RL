360e886fc0883a1e8b13ac87fa883b4199b914ee
Rowing0914 <kosakaboat@gmail.com>
2019-04-06 20:22:23 +0100

    Added

--- agents/Duelling_Double_DQN_PER_model.py
+++ agents/Duelling_Double_DQN_PER_model.py
@@ -204,7 +204,7 @@ def train_Duelling_Double_DQN_PER(main_model, target_model, env, replay_buffer, 
 					next_Q_main = main_model.predict(sess, next_states)
 					next_Q = target_model.predict(sess, next_states)
 					Y = rewards + params.gamma * next_Q[
-						np.arange(params.batch_size), np.argmax(next_Q_main, axis=1)] * dones
+						np.arange(params.batch_size), np.max(next_Q_main, axis=1)] * dones
 
 					# we get a batch of loss for updating PER memory
 					loss, batch_loss = main_model.update(sess, states, actions, Y)
