0662e01db6456f9ee1b45a5991a3c6c76fe87c55
Rowing0914 <kosakaboat@gmail.com>
2019-04-26 15:59:12 +0100

    modified DDPG, yet it does not converge on pendulumn and continuous mountain car....

--- examples/DDPG/DDPG_eager.py
+++ examples/DDPG/DDPG_eager.py
@@ -17,12 +17,13 @@ import gym
 import itertools
 import numpy as np
 import time
+import tensorflow as tf
 from copy import deepcopy
 from collections import deque
 from tf_rl.common.memory import ReplayBuffer
 from tf_rl.common.params import Parameters
 from tf_rl.common.utils import AnnealingSchedule, logging, soft_target_model_update_eager
-import tensorflow as tf
+from tf_rl.common.random_process import OrnsteinUhlenbeckProcess
 
 tf.enable_eager_execution()
 
@@ -35,7 +36,7 @@ class Actor(tf.keras.Model):
 			self.dense1 = tf.keras.layers.Dense(16, activation='relu')
 			self.dense2 = tf.keras.layers.Dense(16, activation='relu')
 			self.dense3 = tf.keras.layers.Dense(16, activation='relu')
-			self.pred = tf.keras.layers.Dense(num_action, activation='tanh')
+			self.pred = tf.keras.layers.Dense(num_action, activation='linear')
 		elif self.env_type == "Atari":
 			self.conv1 = tf.keras.layers.Conv2D(32, kernel_size=8, strides=8, activation='relu')
 			self.conv2 = tf.keras.layers.Conv2D(64, kernel_size=4, strides=2, activation='relu')
@@ -106,12 +107,13 @@ class Actor_Critic:
 		self.target_critic = deepcopy(self.critic)
 		self.actor_optimizer = tf.train.AdamOptimizer()
 		self.critic_optimizer = tf.train.AdamOptimizer()
+		self.random_process = OrnsteinUhlenbeckProcess(size=self.num_action, theta=0.15, mu=0.0, sigma=0.2)
 
 	def predict(self, state, epsilon):
 		if np.random.rand(1) > epsilon:
-			return self.actor(tf.convert_to_tensor(state[None, :], dtype=tf.float32)).numpy()[0]
+			return self.actor(tf.convert_to_tensor(state[None, :], dtype=tf.float32)).numpy()[0]*self.random_process.sample()
 		else:
-			return np.random.uniform(-2.0, 2.0, 1)
+			return np.random.uniform(-2.0, 2.0, 1)*self.random_process.sample()
 
 	def update(self, states, actions, rewards, next_states, dones):
 		"""
@@ -125,21 +127,21 @@ class Actor_Critic:
 			target_actions = self.target_actor(tf.convert_to_tensor(next_states[None, :], dtype=tf.float32))[0]
 			# critic takes as input states, actions so that we combine them before passing them
 			next_q_values = self.target_critic( tf.cast(tf.concat([next_states, target_actions], axis = -1), dtype=tf.float32) )
-			q_values = self.target_critic( tf.cast(tf.concat([states, actions], axis = -1), dtype=tf.float32) )
+			q_values = self.critic( tf.cast(tf.concat([states, actions], axis = -1), dtype=tf.float32) )
 
-			Y = reward + self.params.gamma*next_q_values * np.logical_not(dones)
+			Y = rewards + self.params.gamma*next_q_values * np.logical_not(dones)
 
 			# MSE loss function: (1/N)*sum(Y - Q(s,a))^2
 			critic_loss = tf.reduce_mean(tf.squared_difference(Y, q_values))
 
 		# get gradients
-		critic_grads = tape.gradient(critic_loss, self.target_critic.trainable_weights)
+		critic_grads = tape.gradient(critic_loss, self.critic.trainable_weights)
 
 		# apply processed gradients to the network
-		self.critic_optimizer.apply_gradients(zip(critic_grads, self.target_critic.trainable_weights))
+		self.critic_optimizer.apply_gradients(zip(critic_grads, self.critic.trainable_weights))
 
 		# soft update
-		soft_target_model_update_eager(self.critic, self.target_critic)
+		soft_target_model_update_eager(self.target_critic, self.critic)
 
 		"""
 
@@ -149,24 +151,26 @@ class Actor_Critic:
 
 		with tf.GradientTape() as tape:
 			# compute q-values
-			target_actions = self.target_actor(tf.convert_to_tensor(next_states[None, :], dtype=tf.float32))
-			q_values = self.target_critic( tf.cast(tf.concat([states, target_actions], axis = -1), dtype=tf.float32) )
+			target_actions = self.actor(tf.convert_to_tensor(states[None, :], dtype=tf.float32))
+			q_values = self.target_critic( tf.cast(tf.concat([states[None, :], target_actions], axis = -1), dtype=tf.float32) )
 
-			Y = rewards + self.params.gamma * q_values * np.logical_not(dones)
-
-			actor_loss = tf.reduce_mean(tf.squared_difference(Y, q_values))
+			actor_loss = tf.reduce_mean(-q_values)
 
 		# get gradients
-		actor_grads = tape.gradient(actor_loss, self.target_actor.trainable_weights)
+		actor_grads = tape.gradient(actor_loss, self.actor.trainable_weights)
 
 		# apply processed gradients to the network
-		self.actor_optimizer.apply_gradients(zip(actor_grads, self.target_actor.trainable_weights))
+		self.actor_optimizer.apply_gradients(zip(actor_grads, self.actor.trainable_weights))
 
 		# soft update
-		soft_target_model_update_eager(self.actor, self.target_actor)
+		soft_target_model_update_eager(self.target_actor, self.actor)
+
+		return np.sum(critic_loss + actor_loss)
 
 
-env = gym.make("Pendulum-v0")
+# env = gym.make("Pendulum-v0")
+env = gym.make("MountainCarContinuous-v0")
+
 params = Parameters(algo="DDPG", mode="CartPole")
 agent = Actor_Critic("CartPole", Actor, Critic, 1, params)
 replay_buffer = ReplayBuffer(params.memory_size)
@@ -180,26 +184,29 @@ for i in range(params.num_episodes):
 	memory = list()
 	total_reward = 0
 	start = time.time()
+	agent.random_process.reset_states()
+	action_list = list()
 
 	# generate an episode
 	for t in itertools.count():
-		if i > 200:
-			env.render()
+		# if i > 1000:
+			# env.render()
 		action = agent.predict(state, Epsilon.get_value(i))
 		next_state, reward, done, info = env.step(action)
 		replay_buffer.add(state, action, reward, next_state, done)
 
 		state = next_state
 		total_reward += reward
+		action_list.append(action)
 
-		if done:
+		if done or t > 200:
 			if global_timestep > params.learning_start:
 				states, actions, rewards, next_states, dones = replay_buffer.sample(params.batch_size)
 
 				# update the networks according to the current episode
-				agent.update(states, actions, rewards, next_states, dones)
+				loss = agent.update(states, actions, rewards, next_states, dones)
 
-				logging(global_timestep, params.num_frames, i, time.time() - start, total_reward, 0, Epsilon.get_value(i), [0])
+				logging(global_timestep, params.num_frames, i, time.time() - start, total_reward, loss, Epsilon.get_value(i), [np.mean(np.array(action_list))])
 			total_reward = 0
 			break
 
