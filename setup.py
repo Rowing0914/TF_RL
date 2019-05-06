ea0c193cd245aae762c245cc0a6e95326741404e
Rowing0914 <kosakaboat@gmail.com>
2019-05-06 18:57:58 +0100

    modified numpy array issue in Double DQN

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="TF_RL",
-    version="0.0.21",
+    version="0.0.22",
     author="Norio Kosaka",
     author_email="kosakaboat@gmail.com",
     description="Reinforcement Learning for Tensorflow",
