e588460b76262989648a43c1b614ae675cab8a60
Rowing0914 <kosakaboat@gmail.com>
2019-07-20 21:59:08 +0100

    modified the DDPG to be compatible with extended version of mujoco envs

--- push.sh
+++ push.sh
@@ -3,4 +3,4 @@ echo Hello, what is the comment for this commit?
 
 read comment
 
-git add . && git commit -m "$comment" && git push -u origin experiment
+git add . && git commit -m "$comment" && git push -u origin master
