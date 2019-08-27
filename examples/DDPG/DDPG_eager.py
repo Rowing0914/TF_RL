af9917cae8106fb3028f4738b82d155cf3916ad7
Rowing0914 <kosakaboat@gmail.com>
2019-08-27 16:47:07 +0100

    test

--- examples/DDPG/DDPG_eager.py
+++ examples/DDPG/DDPG_eager.py
@@ -34,9 +34,9 @@ parser.add_argument("--env_name", default="Ant-v2", type=str, help="Env title")
 parser.add_argument("--train_flg", default="original", type=str, help="train flg: original or on-policy")
 # parser.add_argument("--train_flg", default="on-policy", type=str, help="train flg: original or on-policy")
 parser.add_argument("--seed", default=123, type=int, help="seed for randomness")
-# parser.add_argument("--num_frames", default=1_000_000, type=int, help="total frame in a training")
+parser.add_argument("--num_frames", default=1_000_000, type=int, help="total frame in a training")
 # parser.add_argument("--num_frames", default=500_000, type=int, help="total frame in a training")
-parser.add_argument("--num_frames", default=20_000, type=int, help="total frame in a training")
+# parser.add_argument("--num_frames", default=20_000, type=int, help="total frame in a training")
 parser.add_argument("--eval_interval", default=100_000, type=int, help="a frequency of evaluation in training phase")
 # parser.add_argument("--eval_interval", default=5_000, type=int, help="a frequency of evaluation in training phase")
 parser.add_argument("--memory_size", default=100_000, type=int, help="memory size in a training")
@@ -63,13 +63,21 @@ now = datetime.now()
 # params.video_dir = "../../logs/video/video_{}".format(now.strftime("%Y%m%d-%H%M%S") + "_" + str(params.env_name))
 # params.plot_path = "../../logs/plots/plot_{}/".format(now.strftime("%Y%m%d-%H%M%S") + "_" + str(params.env_name))
 
-mu = str(params.mu).split(".")
-mu = str(mu[0]+mu[1])
-params.log_dir = "../../logs/logs/DDPG-{}-seed{}/{}-mu{}".format(params.train_flg, params.seed, str(params.env_name.split("-")[0]), mu)
-params.actor_model_dir = "../../logs/models/DDPG-{}-seed{}/{}/actor-mu{}/".format(params.train_flg, params.seed, str(params.env_name.split("-")[0]), mu)
-params.critic_model_dir = "../../logs/models/DDPG-{}-seed{}/{}/critic-mu{}/".format(params.train_flg, params.seed, str(params.env_name.split("-")[0]), mu)
-params.video_dir = "../../logs/video/DDPG-{}-seed{}/{}-mu{}/".format(params.train_flg, params.seed, str(params.env_name.split("-")[0]), mu)
-params.plot_path = "../../logs/plots/DDPG-{}-seed{}/{}-mu{}/".format(params.train_flg, params.seed, str(params.env_name.split("-")[0]), mu)
+sigma = str(params.sigma).split(".")
+sigma = str(sigma[0] + sigma[1])
+params.log_dir = "../../logs/logs/DDPG-{}-seed{}/{}-sigma{}".format(params.train_flg, params.seed,
+                                                                    str(params.env_name.split("-")[0]), sigma)
+params.actor_model_dir = "../../logs/models/DDPG-{}-seed{}/{}/actor-sigma{}/".format(params.train_flg, params.seed,
+                                                                                     str(params.env_name.split("-")[0]),
+                                                                                     sigma)
+params.critic_model_dir = "../../logs/models/DDPG-{}-seed{}/{}/critic-sigma{}/".format(params.train_flg, params.seed,
+                                                                                       str(
+                                                                                         params.env_name.split("-")[0]),
+                                                                                       sigma)
+params.video_dir = "../../logs/video/DDPG-{}-seed{}/{}-sigma{}/".format(params.train_flg, params.seed,
+                                                                        str(params.env_name.split("-")[0]), sigma)
+params.plot_path = "../../logs/plots/DDPG-{}-seed{}/{}-sigma{}/".format(params.train_flg, params.seed,
+                                                                        str(params.env_name.split("-")[0]), sigma)
 
 env = gym.make(params.env_name)
 env = Monitor(env, params.video_dir)
@@ -81,11 +89,11 @@ tf.random.set_random_seed(params.seed)
 replay_buffer = ReplayBuffer(params.memory_size)
 reward_buffer = deque(maxlen=params.reward_buffer_ep)
 summary_writer = tf.contrib.summary.create_file_writer(params.log_dir)
-random_process = OrnsteinUhlenbeckProcess(size=env.action_space.shape[0], theta=0.15, mu=params.mu, sigma=params.sigma)
-# random_process = GaussianNoise(mu=params.mu, sigma=params.sigma)
+# random_process = OrnsteinUhlenbeckProcess(size=env.action_space.shape[0], theta=0.15, mu=params.mu, sigma=params.sigma)
+random_process = GaussianNoise(mu=params.mu, sigma=params.sigma)
 agent = DDPG(Actor, Critic, env.action_space.shape[0], random_process, params)
 
 if params.train_flg == "original":
-    train_DDPG_original(agent, env, replay_buffer, reward_buffer, summary_writer)
+  train_DDPG_original(agent, env, replay_buffer, reward_buffer, summary_writer)
 elif params.train_flg == "on-policy":
-    train_DDPG_onpolicy(agent, env, replay_buffer, reward_buffer, summary_writer)
+  train_DDPG_onpolicy(agent, env, replay_buffer, reward_buffer, summary_writer)
--- examples/DDPG/exp_noise_insertion.py
+++ examples/DDPG/exp_noise_insertion.py
@@ -3,19 +3,20 @@ import os
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
 ENV_LIST = [
-    "Ant-v2",
-    "HalfCheetah-v2",
-    "Hopper-v2",
-    "Humanoid-v2",
+    # "Ant-v2",
+    # "HalfCheetah-v2",
+    # "Hopper-v2",
+    # "Humanoid-v2",
     "Swimmer-v2",
     "Walker2d-v2"
 ]
 
 for env_name in ENV_LIST:
-    for mu in [0.0, 0.3, 0.6, 0.9]:
+    for sigma in [0.1, 0.3, 0.6, 0.9]:
         # Use 5 seeds at least
         # https://arxiv.org/pdf/1806.08295.pdf
-        for seed in [10, 20, 30, 40, 50]:
-            myCmd = "python3.6 /home/noio0925/Desktop/research/TF_RL/examples/DDPG/DDPG_eager.py " \
-                    "--env_name={} --mu={} --seed={} --train_flg=original".format(env_name, mu, seed)
+        # for seed in [10, 20, 30, 40, 50]:
+        for seed in [10, 20, 30]:
+            myCmd = "python3.6 /home/norio0925/Desktop/TF_RL/examples/DDPG/DDPG_eager.py " \
+                    "--env_name={} --sigma={} --seed={} --train_flg=original".format(env_name, sigma, seed)
             os.system(myCmd)
