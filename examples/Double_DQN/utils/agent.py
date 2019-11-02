c9b15eb1ff9c66497f42023ca5a7c976e9ddebea
Rowing0914 <kosakaboat@gmail.com>
2019-11-02 23:27:47 +0900

    [fix] Fixed DoubleDQN to follow the fixes in DQN

--- examples/Double_DQN/utils/agent.py
+++ examples/Double_DQN/utils/agent.py
@@ -12,7 +12,7 @@ class Double_DQN(object):
                  loss_fn,
                  grad_clip_fn,
                  obs_prc_fn,
-                 dim_action,
+                 num_action,
                  model_dir,
                  gamma):
         self._gamma = gamma
@@ -25,9 +25,9 @@ class Double_DQN(object):
         # === Supposed to access from outside ===
         self.policy = policy
         self.eval_flg = False
-        self.dim_action = dim_action
-        self.main_model = model(dim_action)
-        self.target_model = model(dim_action)
+        self.num_action = num_action
+        self.main_model = model(num_action)
+        self.target_model = model(num_action)
         self.manager = create_checkpoint(model=self.main_model,
                                          optimizer=self._optimizer,
                                          model_dir=model_dir)
@@ -58,28 +58,25 @@ class Double_DQN(object):
 
     # @tf.function
     def _update(self, states, actions, rewards, next_states, dones):
-        # get the current global-timestep
-        self._timestep = tf.compat.v1.train.get_global_step()
-
         # ===== make sure to fit all process to compute gradients within this Tape context!! =====
         with tf.GradientTape() as tape:
-            next_Q_main = self.main_model(next_states)
-            next_Q = self.target_model(next_states)
-            q_values = self.main_model(states)
-            idx_flattened = tf.range(0, tf.shape(next_Q)[0]) * tf.shape(next_Q)[1] + tf.cast(
-                tf.math.argmax(next_Q_main, axis=-1), dtype=tf.int32)
+            q_tp1_main = self.main_model(next_states)  # batch_size x num_action
+            q_tp1 = self.target_model(next_states)  # batch_size x num_action
+            q_t = self.main_model(states)  # batch_size x num_action
+            idx_flattened = tf.range(0, tf.shape(q_tp1)[0]) * tf.shape(q_tp1)[1] + tf.cast(
+                tf.math.argmax(q_tp1_main, axis=-1), dtype=tf.int32)
 
             # passing [-1] to tf.reshape means flatten the array
             # using tf.gather, associate Q-values with the executed actions
-            chosen_next_q = tf.gather(tf.reshape(next_Q, [-1]), idx_flattened)
+            chosen_next_q = tf.gather(tf.reshape(q_tp1, [-1]), idx_flattened)  # (batch_size,)
 
-            Y = rewards + self._gamma * chosen_next_q * (1. - dones)
+            Y = rewards + self._gamma * chosen_next_q * (1. - dones)  # (batch_size,)
             Y = tf.stop_gradient(Y)
 
             # get the q-values which is associated with actually taken actions in a game
-            actions_one_hot = tf.one_hot(actions, self.dim_action, 1.0, 0.0)
-            chosen_q = tf.math.reduce_sum(tf.math.multiply(actions_one_hot, q_values), axis=1)
-            batch_loss = self._loss_fn(Y, chosen_q, reduction=tf.losses.Reduction.NONE)
+            actions_one_hot = tf.one_hot(actions, self.num_action, 1.0, 0.0)  # batch_size x num_action
+            chosen_q = tf.math.reduce_sum(actions_one_hot * q_t, axis=-1)  # (batch_size,)
+            batch_loss = self._loss_fn(Y, chosen_q, reduction=tf.compat.v1.losses.Reduction.NONE)  # (batch_size,)
             loss = tf.math.reduce_mean(batch_loss)
 
         # get gradients
@@ -91,21 +88,12 @@ class Double_DQN(object):
         # apply processed gradients to the network
         self._optimizer.apply_gradients(zip(grads, self.main_model.trainable_weights))
 
-        with tf.name_scope("Agent"):
-            tf.compat.v2.summary.scalar("loss", loss, step=self._timestep)
-            tf.compat.v2.summary.histogram("next_Q(TargetModel)", next_Q, step=self._timestep)
-            tf.compat.v2.summary.histogram("q_values(MainModel)", next_Q, step=self._timestep)
-            tf.compat.v2.summary.histogram("Y(target)", Y, step=self._timestep)
-            tf.compat.v2.summary.scalar("mean_Y(target)", tf.math.reduce_mean(Y), step=self._timestep)
-            tf.compat.v2.summary.scalar("var_Y(target)", tf.math.reduce_variance(Y), step=self._timestep)
-            tf.compat.v2.summary.scalar("max_Y(target)", tf.math.reduce_max(Y), step=self._timestep)
-            tf.compat.v2.summary.scalar("mean_q_value(TargetModel)", tf.math.reduce_mean(next_Q), step=self._timestep)
-            tf.compat.v2.summary.scalar("var_q_value(TargetModel)", tf.math.reduce_variance(next_Q),
-                                        step=self._timestep)
-            tf.compat.v2.summary.scalar("max_q_value(TargetModel)", tf.math.reduce_max(next_Q), step=self._timestep)
-            tf.compat.v2.summary.scalar("mean_q_value(MainModel)", tf.math.reduce_mean(q_values), step=self._timestep)
-            tf.compat.v2.summary.scalar("var_q_value(MainModel)", tf.math.reduce_variance(q_values),
-                                        step=self._timestep)
-            tf.compat.v2.summary.scalar("max_q_value(MainModel)", tf.math.reduce_max(q_values), step=self._timestep)
+        # get the current global time-step
+        ts = self._timestep = tf.compat.v1.train.get_global_step()
+        tf.compat.v2.summary.scalar("agent/loss", loss, step=ts)
+        tf.compat.v2.summary.scalar("agent/mean q_tp1 - q_t", tf.math.reduce_mean(q_tp1 - q_t), step=ts)
+        tf.compat.v2.summary.scalar("agent/mean Y", tf.math.reduce_mean(Y), step=ts)
+        tf.compat.v2.summary.scalar("agent/mean q_tp1", tf.math.reduce_mean(q_tp1), step=ts)
+        tf.compat.v2.summary.scalar("agent/mean q_t", tf.math.reduce_mean(q_t), step=ts)
 
         return loss, batch_loss
