2bd659f9bc977ded9fa8ed3816b61081c721c567
Rowing0914 <kosakaboat@gmail.com>
2019-11-09 19:27:42 +0900

    colab dev

--- tf_rl/common/set_up.py
+++ tf_rl/common/set_up.py
@@ -3,8 +3,13 @@ import datetime
 from tf_rl.common.abs_path import ROOT_DIR as ROOT
 
 
-def set_up_for_training(env_name, seed, gpu_id, log_dir="Test", prev_log=""):
+def set_up_for_training(env_name, seed, gpu_id, log_dir="Test", prev_log="", google_colab=False):
     os.environ["CUDA_VISIBLE_DEVICES"] = str(gpu_id)
+
+    if google_colab:
+        # mount your drive on google colab
+        from google.colab import drive
+        drive.mount("/content/gdrive")
 
     if prev_log == "":
         exp_date = datetime.datetime.now().strftime("%Y%m%d%H%M%S")  # dir_name format
